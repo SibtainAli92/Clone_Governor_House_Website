99ef6adc73cbe357451619514e60098e1b7c19e8
Sibtain Ali <hellofozi@gmail.com>
2025-01-04 00:11:50 +0500

    first commit

---
+++ Governor-Website-Clone-main/tailwind.config.ts
@@ -0,0 +1,18 @@
+import type { Config } from "tailwindcss";
+
+export default {
+  content: [
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        main: "#044e83",
+        sub: "#2eb6e8",
+      },
+    },
+  },
+  plugins: [],
+} satisfies Config;
